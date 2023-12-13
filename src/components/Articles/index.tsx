b42ff958597d2612c01b71d2387547bd586a5152
MichaelChuya <michaelchuyapay@gmail.com>
2023-12-13 01:16:41 -0400

    Se agrega sem_15

--- src/components/Articles/index.tsx
+++ src/components/Articles/index.tsx
@@ -1,77 +1,24 @@
 
-import styles from "./styles.module.scss";
-import React from "react";
+import styles from './styles.module.scss';
+import React from 'react';
+import { volumesData } from './mockData';
 
-const Articles = () => {
+const Articles: React.FC = () => {
     return(
         <div >
             <div className={styles.titulo}>VOLÚMENES</div>
             <div className={styles.container}>
-                <div className={styles.article}>
-                    <article className={styles.image}>
-                        <a href=""><img src="images1.jpg" alt="" /></a>
-                        <p className={styles.text}>Abril 15 | 2023</p>
-                        <p className={styles.vol}>Vol.1</p>
-                    </article>
-                </div>
-                <div className={styles.article}>
-                    <article className={styles.image}>
-                        <a href=""><img src="images2.jpg" alt="" /></a>
-                        <p className={styles.text}>Abril 18 | 2023</p>
-                        <p className={styles.vol}>Vol.2</p>
-                    </article>
-                </div>
-                <div className={styles.article}>
-                    <article className={styles.image}>
-                        <a href=""><img src="images3.jpg" alt="" /></a>
-                        <p className={styles.text}>Abril 22 | 2023</p>
-                        <p className={styles.vol}>Vol.3</p>
-                    </article>
-                </div>
-                <div className={styles.article}>
-                    <article className={styles.image}>
-                        <a href=""><img src="images1.jpg" alt="" /></a>
-                        <p className={styles.text}>Abril 24 | 2023</p>
-                        <p className={styles.vol}>Vol.4</p>
-                    </article>
-                </div>
-                <div className={styles.article}>
-                    <article className={styles.image}>
-                        <a href=""><img src="images2.jpg" alt="" /></a>
-                        <p className={styles.text}>Abril 25 | 2023</p>
-                        <p className={styles.vol}>Vol.5</p>
-                    </article>
-                </div>
-                <div className={styles.article}>
-                    <article className={styles.image}>
-                        <a href=""><img src="images3.jpg" alt="" /></a>
-                        <p className={styles.text}>Abril 27 | 2023</p>
-                        <p className={styles.vol}>Vol.6</p>
-                    </article>
-                </div>
-                <div className={styles.article}>
-                    <article className={styles.image}>
-                        <a href=""><img src="images1.jpg" alt="" /></a>
-                        <p className={styles.text}>Abril 28 | 2023</p>
-                        <p className={styles.vol}>Vol.7</p>
-                    </article>
-                </div>
-                <div className={styles.article}>
-                    <article className={styles.image}>
-                        <a href=""><img src="images2.jpg" alt="" /></a>
-                        <p className={styles.text}>Abril 29 | 2023</p>
-                        <p className={styles.vol}>Vol.8</p>
-                    </article>
-                </div>
-                <div className={styles.article}>
-                    <article className={styles.image}>
-                        <a href=""><img src="images3.jpg" alt="" /></a>
-                        <p className={styles.text}>Abril 30 | 2023</p>
-                        <p className={styles.vol}>Vol.9</p>
-                    </article>
-                </div>
+                {volumesData && volumesData.map((volume) => (
+                    <div key={volume.id} className={styles.article}>
+                        <article className={styles.image}>
+                            <a href=""><img src={volume.imageUrl} alt="" /></a>
+                            <p className={styles.text}>{volume.date}</p>
+                            <p className={styles.vol}>{volume.volumeNumber}</p>
+                        </article>
+                    </div>
+                ))}
             </div>
         </div>
-    )
-}
-export default Articles
+    );
+};
+export default Articles;
---
+++ src/components/Articles/mockData.js
@@ -0,0 +1,56 @@
+export const volumesData = [
+    {
+        id: 1,
+        imageUrl: 'Rectangle 27.png',
+        date: 'Abril 15 | 2023',
+        volumeNumber: 'Vol.1',
+    },
+    {
+        id: 2,
+        imageUrl: 'Rectangle 28.png',
+        date: 'Abril 15 | 2023',
+        volumeNumber: 'Vol.2',
+    },
+    {
+        id: 3,
+        imageUrl: 'Rectangle 29.png',
+        date: 'Abril 15 | 2023',
+        volumeNumber: 'Vol.3',
+    },
+    {
+        id: 4,
+        imageUrl: 'Rectangle 30.png',
+        date: 'Abril 15 | 2023',
+        volumeNumber: 'Vol.4',
+    },
+    {
+        id: 5,
+        imageUrl: 'Rectangle 31.png',
+        date: 'Abril 15 | 2023',
+        volumeNumber: 'Vol.5',
+    },
+    {
+        id: 6,
+        imageUrl: 'Rectangle 32.png',
+        date: 'Abril 15 | 2023',
+        volumeNumber: 'Vol.6',
+    },
+    {
+        id: 7,
+        imageUrl: 'Rectangle 33.png',
+        date: 'Abril 15 | 2023',
+        volumeNumber: 'Vol.7',
+    },
+    {
+        id: 8,
+        imageUrl: 'Rectangle 30.png',
+        date: 'Abril 15 | 2023',
+        volumeNumber: 'Vol.8',
+    },
+    {
+        id: 9,
+        imageUrl: 'Rectangle 27.png',
+        date: 'Abril 15 | 2023',
+        volumeNumber: 'Vol.9',
+    },
+];
